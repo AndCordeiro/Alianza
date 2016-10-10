c93f0e39d364d2120b48cfa8ba64be63fdbb36d3
AndCordeiro <andrecordeiro.oliveira@hotmail.com>
2016-10-10 16:36:12 -0300

    Second Commit by Android Studio

---
+++ Alianza/app/src/main/java/com/example/alianza/activity/RegisterCreateNewsActivity.java
@@ -0,0 +1,15 @@
+package com.example.alianza.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alianza.R;
+
+public class RegisterCreateNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_create_news);
+    }
+}
